68de1dfd19c57170809d4906f426ffb36d3e748c
Sypermax <irmax121@gmail.com>
2020-06-27 09:23:42 +0300

    Changed island

--- island.py
+++ island.py
@@ -1,8 +1,2 @@
-def missing_elements(L):
-    start, end = L[0], L[-1]
-    return sorted(set(range(start, end + 1)).difference(L))
-def find_missing_letter(chars):
-    numbers = list(map(ord, chars))
-    n = missing_elements(numbers)
-    return chr(n[0])
-print(find_missing_letter(['a','b','c','d','f']))
+filter_list = lambda l: [x for x in l if type(x) != str]
+print(filter_list([1,2,'aasf','1','123',123]))
